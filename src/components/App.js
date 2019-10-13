779946b703e755c67c1782d193e847933cee7630
Farkas Antal <farkas.antal@autsoft.hu>
2019-10-13 21:00:08 +0200

    Add providers

--- src/components/App.js
+++ src/components/App.js
@@ -6,46 +6,62 @@ import {
   Switch
 } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { useTheme } from '../hooks/theme';
 import { darkTheme, lightTheme } from '../themes';
 import { Button, Box } from './common';
 import NavItem from './common/NavItem';
 import Favourites from './Favourites';
+import FavouritesProvder from './FavouritesProvider';
 import GlobalStyle from './GlobalStyle';
 import Home from './Home';
+import {
+  default as AppThemeProvider,
+  ThemeContext as AppThemeContext
+} from './ThemeProvider';
 
 function App() {
-  const [theme, toggleTheme] = useTheme();
-
   return (
-    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-      <>
-        <GlobalStyle />
-        <Router>
-          <>
-            <Box display="flex" alignItems="center" p={2}>
-              <NavItem to="/" activeClassName="active" exact>
-                Home
-              </NavItem>
-              <NavItem to="/favourites" activeClassName="active">
-                Favourites
-              </NavItem>
-              <Box flex="1" />
-              <Button onClick={() => toggleTheme()}>{theme}</Button>
-            </Box>
-            <Switch>
-              <Route path="/" exact>
-                <Home />
-              </Route>
-              <Route path="/favourites">
-                <Favourites />
-              </Route>
-              <Redirect to="/" />
-            </Switch>
-          </>
-        </Router>
-      </>
-    </ThemeProvider>
+    <FavouritesProvder>
+      <AppThemeProvider>
+        <AppThemeContext.Consumer>
+          {([theme, setTheme]) => (
+            <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+              <>
+                <GlobalStyle />
+                <Router>
+                  <>
+                    <Box display="flex" alignItems="center" p={2}>
+                      <NavItem to="/" activeClassName="active" exact>
+                        Home
+                      </NavItem>
+                      <NavItem to="/favourites" activeClassName="active">
+                        Favourites
+                      </NavItem>
+                      <Box flex="1" />
+                      <Button
+                        onClick={() =>
+                          setTheme(theme === 'dark' ? 'light' : 'dark')
+                        }
+                      >
+                        {theme}
+                      </Button>
+                    </Box>
+                    <Switch>
+                      <Route path="/" exact>
+                        <Home />
+                      </Route>
+                      <Route path="/favourites">
+                        <Favourites />
+                      </Route>
+                      <Redirect to="/" />
+                    </Switch>
+                  </>
+                </Router>
+              </>
+            </ThemeProvider>
+          )}
+        </AppThemeContext.Consumer>
+      </AppThemeProvider>
+    </FavouritesProvder>
   );
 }
 
