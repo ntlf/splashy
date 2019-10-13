3791428fe4d6e239848e889d72d7ad10c79f2cac
Farkas Antal <farkas.antal@autsoft.hu>
2019-10-13 16:49:23 +0200

    Load photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { UnsplashAPI } from '../api/index';
+import Gallery from './Gallery';
 
 function Home() {
-  return <div>Home</div>;
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await UnsplashAPI.getPhotos(1);
+
+      setPhotos(data);
+    };
+
+    fetch();
+  }, []);
+
+  return (
+    <div>
+      <Gallery photos={photos} />
+    </div>
+  );
 }
 
 export default Home;
