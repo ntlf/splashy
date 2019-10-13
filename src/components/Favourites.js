e2a942de5f1642f91db4e619f6cfdc4ede51cba2
Farkas Antal <farkas.antal@autsoft.hu>
2019-10-13 18:23:49 +0200

    Add favourites gallery

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useFavourites } from '../hooks/favourites';
+import { UnsplashAPI } from '../api/index';
+import Gallery from './Gallery';
 
 function Favourites() {
-  return <div>Favourites</div>;
+  const { favourites, remove } = useFavourites();
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const promises = favourites.map(id => UnsplashAPI.getPhoto(id));
+
+      const data = await Promise.all(promises);
+
+      setPhotos(data);
+    };
+
+    fetch();
+  }, [favourites]);
+
+  return (
+    <div>
+      {!favourites.length && <div>Your favourites list is empty!</div>}
+      <Gallery
+        photos={photos}
+        onPhotoClick={id => remove(id)}
+        renderHoverEmoji={() => '💔'}
+      />
+    </div>
+  );
 }
 
 export default Favourites;
