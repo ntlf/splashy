3a7471171fb58a67b23f61d9cb49b7038f2ab02c
Farkas Antal <farkas.antal@autsoft.hu>
2019-10-13 19:00:45 +0200

    Add dark and light theme

---
+++ src/themes/dark.js
@@ -0,0 +1,13 @@
+const dark = {
+  colors: {
+    textColor: '#ECEFF4',
+    background: '#2E3440',
+    buttonColor: '#2E3440',
+    buttonBackground: '#D09870',
+    inputColor: '#2E3440',
+    inputBackground: '#ECEFF4',
+    linkColor: '#D08770'
+  }
+};
+
+export default dark;
---
+++ src/themes/light.js
@@ -0,0 +1,13 @@
+const light = {
+  colors: {
+    textColor: '#2E3440',
+    background: '#ECEFF4',
+    buttonColor: '#ECEFF4',
+    buttonBackground: '#5E81AC',
+    inputColor: '#2E3440',
+    inputBackground: '#FFF',
+    linkColor: '#5E81AC'
+  }
+};
+
+export default light;
