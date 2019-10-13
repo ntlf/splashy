4ee0b98976fb67773f8d086a7c7639557c90c38c
Farkas Antal <farkas.antal@autsoft.hu>
2019-10-13 19:46:15 +0200

    Add box

---
+++ src/components/common/Box.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { color, layout, space, typography, flexbox } from 'styled-system';
+
+const Box = styled('div')`
+  ${space}
+  ${layout}
+  ${color}
+  ${typography}
+  ${flexbox}
+`;
+
+export default Box;
